9d376d2a56154df638931e620731b737aa09e172
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-07-31 18:22:10 -0300

    commit referente ao artefato persistencia.
    Adicionada a interface dao ao artefato persistencia.
    
    -> Interface que irá prover funções pré definidas de acesso ao banco de
    dados.

---
+++ persistencia/src/main/java/br/com/codetisolutions/persistencia/DAO.java
@@ -0,0 +1,127 @@
+package br.com.codetisolutions.persistencia;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> DAO.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b> Interface provedora das funções pré definidas do acesso ao banco de dados.
+ * </p>
+ *
+ * Data de criação: 31/07/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public interface DAO {
+
+	/**
+	 * Método responsável por buscar por uma entidade através de seu identificador único.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param identificador
+	 *            - id do registro.
+	 * 
+	 * @return <i>entidade pesquisada</i>
+	 */
+	E obter(final Serializable identificador);
+
+	/**
+	 * Método responsável por salvar ou alterar um registro na base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 *            - objeto que será salvo ou alterado.
+	 */
+	void salvarOuAlterar(final E entidade);
+
+	/**
+	 * Método responsável por inserir um registro na base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 *            - objeto que será salvo/inserido.
+	 * 
+	 * @return <i>identificador da entidade inserida</i>
+	 */
+	Serializable inserir(final E entidade);
+
+	/**
+	 * Método responsável por alterar um objeto da base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 *            - objeto que será alterada.
+	 */
+	void alterar(final E entidade);
+
+	/**
+	 * Método responsável por mesclar um objeto da base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 */
+	void mesclar(final E entidade);
+
+	/**
+	 * Método responsável por remover um objeto da base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 *            - objeto que será removido.
+	 */
+	void remover(final E entidade);
+
+	/**
+	 * Método responsável por remover uma lista de objetos da base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param colecaoDados
+	 *            - lista de objetos que serão removidas.
+	 */
+	void removerTodos(final Collection<E> colecaoDados);
+
+	/**
+	 * Método responsável por pesquisar através de um filtro.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 *            - filtro da consulta.
+	 * 
+	 * @return <i>coleção de dados do objeto pesquisado</i>
+	 */
+	Collection<E> consultar(final E entidade);
+
+	/**
+	 * Método responsável por pesquisar sem filtro.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @return <i>toda a coleção de dados do objeto da lista</i>
+	 */
+	Collection<E> consultarTodos();
+
+	/**
+	 * Método responsável por sincronizar a entidade com a base de dados.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param entidade
+	 *            - objeto que será atualizado.
+	 */
+	void atualizar(final E entidade);
+}
