0064873d6641510dfee0985906dcd80ddb225230
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-07-31 18:37:43 -0300

    Classe abstrata BO adicionada no artefato objeto-negocio.
    Adicionada a classe abstrata que irá encapsular a camada de persistência
    e prover funções CRUD porém com as validações básicas implementadas.

---
+++ objeto-negocio/src/main/java/br/com/codetisolutions/negocio/BO.java
@@ -0,0 +1,24 @@
+package br.com.codetisolutions.negocio;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> BO.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b> Classe abstrata responsável pela validação das regras de negócio que estão vinculadas a entidade.
+ * </p>
+ *
+ * Data de criação: 31/07/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class BO {
+
+}
