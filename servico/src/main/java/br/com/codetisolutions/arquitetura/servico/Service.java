747e203f65a9a3ff025a641bdb039e62e00f826b
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-07-31 18:59:41 -0300

    commit referente ao artefato servico.
    Adicionado um novo comentário para a classe abstrata Service.
    
    Inclusão de novas dependências de artefatos no arquivo pom.xml do
    artefato servico.

--- servico/src/main/java/br/com/codetisolutions/arquitetura/servico/Service.java
+++ servico/src/main/java/br/com/codetisolutions/arquitetura/servico/Service.java
@@ -1,5 +1,24 @@
 package br.com.codetisolutions.arquitetura.servico;
 
-public class Service {
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> Service.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 31/07/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public abstract class Service {
 
 }
