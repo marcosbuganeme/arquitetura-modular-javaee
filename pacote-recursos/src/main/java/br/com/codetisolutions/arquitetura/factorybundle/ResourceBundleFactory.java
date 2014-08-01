97b1ae25b3cd099897a4f531bbb2fdc66cad1232
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-01 18:44:56 -0300

    commit referente ao artefato pacote-recursos
    Implementação da classe ResourceBundleFactory do artefato
    pacote-recursos.

--- pacote-recursos/src/main/java/br/com/codetisolutions/arquitetura/factorybundle/ResourceBundleFactory.java
+++ pacote-recursos/src/main/java/br/com/codetisolutions/arquitetura/factorybundle/ResourceBundleFactory.java
@@ -1,5 +1,8 @@
 package br.com.codetisolutions.arquitetura.factorybundle;
 
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
 /**
  * <p>
  * <b>Organização:</b> 4code TI Solutions
@@ -10,7 +13,7 @@ package br.com.codetisolutions.arquitetura.factorybundle;
  * </p>
  *
  * <p>
- * <b>Descrição:</b>
+ * <b>Descrição:</b> Classe responsável pela determinação de qual <code>ResourceBundle</code> será obtida as mensagens do sistema.
  * </p>
  *
  * Data de criação: 01/08/2014
@@ -19,6 +22,39 @@ package br.com.codetisolutions.arquitetura.factorybundle;
  *
  * @version 1.0.0
  */
-public class ResourceBundleFactory {
+public abstract class ResourceBundleFactory {
+
+	/**
+	 * Método responsável por criar o resourceBundle para a aplicação.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @return <i>resiyrceBundle para a aplicação</i>.
+	 */
+	public abstract ResourceBundle getResourceBundle();
+
+	/**
+	 * Método responsável por obter o resourceBundle a partir do factory parametrizado.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param factoryClass
+	 *            - classe do factory que irá prover a criação do resourceBundle.
+	 * 
+	 * @return <i>resourceBundle para a aplicação</i>.
+	 */
+	public static ResourceBundle getBundle(final Class<? extends ResourceBundleFactory> factoryClass) {
+
+		try {
+
+			return factoryClass.newInstance().getResourceBundle();
+
+		} catch (final Exception e) {
+
+			Logger.getLogger("factory.resourceBundle.ResourceBundleFactory").info(" ***** ERROR ***** : " + e.getMessage());
+		}
+
+		return null;
+	}
 
 }
