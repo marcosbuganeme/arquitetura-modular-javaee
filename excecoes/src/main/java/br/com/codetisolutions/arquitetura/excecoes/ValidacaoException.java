21648b309efd85bed4906f0a6acf0fc3f953f586
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-02 01:51:17 -0300

    criação das classes do artefato excecoes.
    -> Adição de novas classes para o artefato.

---
+++ excecoes/src/main/java/br/com/codetisolutions/arquitetura/excecoes/ValidacaoException.java
@@ -0,0 +1,24 @@
+package br.com.codetisolutions.arquitetura.excecoes;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> ValidacaoException.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 02/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class ValidacaoException {
+
+}
