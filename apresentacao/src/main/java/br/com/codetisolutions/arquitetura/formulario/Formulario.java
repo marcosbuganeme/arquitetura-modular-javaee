87558a1390d2ad9a4c2f1500fb7de59411c04433
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-04 14:52:09 -0300

    commit referente ao artefato apresentacao.
    -> Adição de comentários
    
    -> Adição da descrição da classe.
    
    -> Adição de generics para recuperar o tipo da entidade.
    
    -> Adição de novos construtores para facilitar a reusabilidade de
    código.
    
    -> Adicionado atributos para controle de sessão.

--- apresentacao/src/main/java/br/com/codetisolutions/arquitetura/formulario/Formulario.java
+++ apresentacao/src/main/java/br/com/codetisolutions/arquitetura/formulario/Formulario.java
@@ -1,5 +1,11 @@
 package br.com.codetisolutions.arquitetura.formulario;
 
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.logging.Logger;
+
 import br.com.codetisolutions.arquitetura.dominio.Entidade;
 
 /**
@@ -12,7 +18,7 @@ import br.com.codetisolutions.arquitetura.dominio.Entidade;
  * </p>
  *
  * <p>
- * <b>Descrição:</b>
+ * <b>Descrição:</b> Classe responsável pela manutenção da <code>E - Entidade</code> manipulada pela classa concreta, armazenando os dados vindos da camada de visão.
  * </p>
  *
  * Data de criação: 01/08/2014
@@ -24,6 +30,132 @@ import br.com.codetisolutions.arquitetura.dominio.Entidade;
  *
  * @version 1.0.0
  */
-public class Formulario<E extends Entidade> {
+@SuppressWarnings("unchecked")
+public class Formulario<E extends Entidade> implements Serializable {
+
+	/** Constante serialVersionUID. */
+	private static final long serialVersionUID = -5606821856286575455L;
+
+	/** Atributo entidade. */
+	private E entidade;
+
+	/** Atributo consulta. */
+	private E consulta;
+
+	/** Atributo colecaoEntidades. */
+	private Collection<E> colecaoEntidades;
+
+	/**
+	 * Responsável pela criação de novas instâncias desta classe.
+	 */
+	public Formulario( final Class<E> clazz ) {
+
+		try {
+
+			this.entidade = clazz.newInstance();
+
+			this.consulta = clazz.newInstance();
+
+		} catch (Exception e) {
+
+			Logger.getLogger(this.getClass().getName()).info("***** ERROR : " + e.getMessage() + " *****");
+		}
+
+	}
+
+	/**
+	 * Responsável pela criação de novas instâncias desta classe.
+	 * 
+	 * <br>
+	 * 
+	 * Cria o formulário atribuindo as classes de manutenção o tipo genérico informado.
+	 */
+	public <F> Formulario() {
+
+		try {
+
+			this.entidade = this.getTipoDaEntidade().newInstance();
+
+			this.consulta = this.getTipoDaEntidade().newInstance();
+
+		} catch (Exception e) {
+
+			Logger.getLogger(this.getClass().getName()).info("***** ERROR : " + e.getMessage() + " *****");
+		}
+	}
+
+	/**
+	 * Método responsável por capturar o tipo da entidade através de generics.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @return <code>o tipo da entidade</code>.
+	 */
+	private Class<E> getTipoDaEntidade() {
+
+		Type[] tipoEntidade = ( (ParameterizedType) this.getClass().getGenericSuperclass() ).getActualTypeArguments();
+
+		return (Class<E>) tipoEntidade[0];
+	}
+
+	/**
+	 * Retorna o valor do atributo <code>entidade</code>
+	 *
+	 * @return <code>E</code>
+	 */
+	public E getEntidade() {
+
+		return entidade;
+	}
+
+	/**
+	 * Define o valor do atributo <code>entidade</code>.
+	 *
+	 * @param entidade
+	 */
+	public void setEntidade(E entidade) {
+
+		this.entidade = entidade;
+	}
+
+	/**
+	 * Retorna o valor do atributo <code>consulta</code>
+	 *
+	 * @return <code>E</code>
+	 */
+	public E getConsulta() {
+
+		return consulta;
+	}
+
+	/**
+	 * Define o valor do atributo <code>consulta</code>.
+	 *
+	 * @param consulta
+	 */
+	public void setConsulta(E consulta) {
+
+		this.consulta = consulta;
+	}
+
+	/**
+	 * Retorna o valor do atributo <code>colecaoEntidades</code>
+	 *
+	 * @return <code>Collection<E></code>
+	 */
+	public Collection<E> getColecaoEntidades() {
+
+		return colecaoEntidades;
+	}
+
+	/**
+	 * Define o valor do atributo <code>colecaoEntidades</code>.
+	 *
+	 * @param colecaoEntidades
+	 */
+	public void setColecaoEntidades(Collection<E> colecaoEntidades) {
+
+		this.colecaoEntidades = colecaoEntidades;
+	}
 
 }
