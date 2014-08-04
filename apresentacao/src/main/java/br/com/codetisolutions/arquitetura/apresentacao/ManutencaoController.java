01cd7f5ebdf1b916bbca997c5573612285906414
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-04 14:50:27 -0300

    commit referente ao artefato apresentacao.
    -> Enpsulamento de funções.
    
    -> Adição de generics para recuperar o tipo da entidade.
    
    -> Adicionado funções pré definidas para a recuperação de dados.
    
    -> Adicionado o uso do modificador abstract na classe.

--- apresentacao/src/main/java/br/com/codetisolutions/arquitetura/apresentacao/ManutencaoController.java
+++ apresentacao/src/main/java/br/com/codetisolutions/arquitetura/apresentacao/ManutencaoController.java
@@ -12,7 +12,7 @@ import br.com.codetisolutions.arquitetura.dominio.Entidade;
  * </p>
  *
  * <p>
- * <b>Descrição:</b>
+ * <b>Descrição:</b> Classe abstrata responsável pelo fluxo de navegação do sistema, disponibilizando funções básicas para manutenção de registros.
  * </p>
  *
  * Data de criação: 01/08/2014
@@ -24,6 +24,160 @@ import br.com.codetisolutions.arquitetura.dominio.Entidade;
  *
  * @version 1.0.0
  */
-public class ManutencaoController<E extends Entidade> {
+public abstract class ManutencaoController<E extends Entidade> extends ConsultaController<E> {
+
+	/**
+	 * Método responsável por abrir a página de inclusão do caso de uso.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @return <i>página de inclusão do caso de uso</i>. <br>
+	 *         Exemplo de retorno do método --> EmitirRelatorioController + "/incluir".
+	 */
+	public abstract String abreIncluir();
+
+	/**
+	 * Método responsável por abrir a página de alteração do caso de uso.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @return <i>página de alteração do caso de uso</i>. <br>
+	 *         Exemplo de retorno do método --> EmitirRelatorioController + "/alterar".
+	 */
+	public abstract String abreAlterar();
+
+	/**
+	 * Método responsável por exibir mensagens de exceção na tela.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param e
+	 *            - exceção que será lançada.
+	 */
+	private void exibirMensagemExcecaoTela(final Exception e) {
+
+		this.exibirMensagemNaTela(this.getMessages(e.getMessage()));
+	}
+
+	/**
+	 * Método responsável por realizar a inclusão ou alteração da entidade configurada no formulario.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 */
+	public void salvarOuAlterar() {
+
+		try {
+
+			final String mensagemSalvarOuAlterar = this.getService().salvarOuAlterar(this.getFormulario().getEntidade());
+
+			this.exibirMensagemNaTela(mensagemSalvarOuAlterar);
+
+		} catch (Exception e) {
+
+			this.exibirMensagemExcecaoTela(e);
+		}
+
+	}
+
+	/**
+	 * Método responsável por realizar a inserção da entidade configurada no formulario.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 */
+	public void inserir() {
+
+		try {
+
+			final String mensagemInserir = this.getService().inserir(this.getFormulario().getEntidade());
+
+			this.exibirMensagemNaTela(mensagemInserir);
+
+		} catch (Exception e) {
+
+			this.exibirMensagemExcecaoTela(e);
+		}
+	}
+
+	/**
+	 * Método responsável por realizar a alteração da entidade configurada no formulario.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 */
+	public void alterar() {
+
+		try {
+
+			final String mensagemAlterar = this.getService().alterar(this.getFormulario().getEntidade());
+
+			this.exibirMensagemNaTela(mensagemAlterar);
+
+		} catch (Exception e) {
+
+			this.exibirMensagemExcecaoTela(e);
+		}
+	}
+
+	/**
+	 * Método responsável por realizar a mesclagem da entidade configurada no formulario.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 */
+	public void mesclar() {
+
+		try {
+
+			final String mensagemMesclar = this.getService().mesclar(this.getFormulario().getEntidade());
+
+			this.exibirMensagemNaTela(mensagemMesclar);
+
+		} catch (Exception e) {
+
+			this.exibirMensagemExcecaoTela(e);
+		}
+	}
+
+	/**
+	 * Método responsável por realizar a remoção da entidade configurada no formulario.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 */
+	public void remover() {
+
+		try {
+
+			final String mensagemRemover = this.getService().remover(this.getFormulario().getEntidade());
+
+			this.exibirMensagemNaTela(mensagemRemover);
+
+		} catch (Exception e) {
+
+			this.exibirMensagemExcecaoTela(e);
+		}
+	}
+
+	/**
+	 * Método responsável por realizar a remoção da coleção de entidades configurada no formulario.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 */
+	public void removerTodos() {
+
+		try {
+
+			final String mensagemRemoverTodos = this.getMessage(this.getService().removerTodos(this.getFormulario().getColecaoEntidades()));
+
+			this.exibirMensagemNaTela(mensagemRemoverTodos);
+
+		} catch (Exception e) {
+
+			this.exibirMensagemExcecaoTela(e);
+		}
+	}
 
 }
