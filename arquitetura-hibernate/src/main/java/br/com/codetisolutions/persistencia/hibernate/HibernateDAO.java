9d79f196b739ece741d7954d0c5b2c806753a07a
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-04 21:47:49 -0300

    commit referente ao artefato arquitetura-hibernate.
    -> criação das classes da arquitetura-hibernate.

---
+++ arquitetura-hibernate/src/main/java/br/com/codetisolutions/persistencia/hibernate/HibernateDAO.java
@@ -0,0 +1,24 @@
+package br.com.codetisolutions.persistencia.hibernate;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> HibernateDAO.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 04/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class HibernateDAO {
+
+}
