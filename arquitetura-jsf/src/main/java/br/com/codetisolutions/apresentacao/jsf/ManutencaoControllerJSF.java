e23ff3619482cb0408e4482b64e079de1aedd37f
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-04 21:42:40 -0300

    commit referente ao artefato arquitetura-jsf.
    -> criação das novas classes.

---
+++ arquitetura-jsf/src/main/java/br/com/codetisolutions/apresentacao/jsf/ManutencaoControllerJSF.java
@@ -0,0 +1,24 @@
+package br.com.codetisolutions.apresentacao.jsf;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> ManutencaoControllerJSF.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 04/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class ManutencaoControllerJSF {
+
+}
