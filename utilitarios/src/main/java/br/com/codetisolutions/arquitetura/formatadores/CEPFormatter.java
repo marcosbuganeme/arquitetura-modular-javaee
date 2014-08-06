9708cdfefb4c6d5482772785929859f6bcf2cbbf
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-06 11:25:23 -0300

    commit referente ao artefato utilitarios.
    -> Criação da classe CEPFormatter.
    
    -> Adição da nova implementação.
    
    -> Refatoração do código.
    
    -> Adição de comentários e descrição dos métodos/escopo da classe.

--- utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/CEPFormatter.java
+++ utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/CEPFormatter.java
@@ -1,5 +1,7 @@
 package br.com.codetisolutions.arquitetura.formatadores;
 
+import br.com.codetisolutions.arquitetura.pattern.FormatacaoPattern;
+
 /**
  * <p>
  * <b>Organização:</b> 4code TI Solutions
@@ -10,7 +12,7 @@ package br.com.codetisolutions.arquitetura.formatadores;
  * </p>
  *
  * <p>
- * <b>Descrição:</b>
+ * <b>Descrição:</b> Classe responsável pela formatação de CEPS.
  * </p>
  *
  * Data de criação: 06/08/2014
@@ -19,6 +21,53 @@ package br.com.codetisolutions.arquitetura.formatadores;
  *
  * @version 1.0.0
  */
-public class CEPFormatter {
+public class CEPFormatter extends Formatter {
 
+	/** Atributo TAMANHO_CEP. */
+	private static final int TAMANHO_CEP = 8;
+
+	/**
+	 * Responsável pela criação de novas instâncias desta classe.
+	 */
+	public CEPFormatter() {
+
+		super();
+	}
+
+	/**
+	 * Método responsável por formatar o cep de acordo com o pattern passado.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param cep
+	 *            - cep que será formatado.
+	 * 
+	 * @return <i>cep formatado</i>.
+	 */
+	public static String formatarCep(final String cep) {
+
+		String resultadoFormatacaoCep = null;
+
+		if (CEPFormatter.isCepValido(cep)) {
+
+			resultadoFormatacaoCep = Formatter.formatarString(cep, FormatacaoPattern.getCEP());
+		}
+
+		return resultadoFormatacaoCep;
+	}
+
+	/**
+	 * Método responsável por validar um cep.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param cep
+	 *            - que será validado.
+	 * 
+	 * @return <i>cep validado</i>.
+	 */
+	public static boolean isCepValido(final String cep) {
+
+		return ( ( cep != null ) && ( cep.length() == CEPFormatter.TAMANHO_CEP ) );
+	}
 }
