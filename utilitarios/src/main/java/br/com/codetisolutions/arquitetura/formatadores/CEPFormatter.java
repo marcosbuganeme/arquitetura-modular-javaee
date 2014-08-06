97dec2d0e116d49e678687ec0baed4651b68e773
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-06 09:34:30 -0300

    commit referente ao artefato
    -> Adição da classe CEPFormatter.

---
+++ utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/CEPFormatter.java
@@ -0,0 +1,25 @@
+package br.com.codetisolutions.arquitetura.formatadores;
+
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> CEPFormatter.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 06/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class CEPFormatter {
+
+}
