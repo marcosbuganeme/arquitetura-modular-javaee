c7103ae69c79bfa73bcb41a2a6d58a14edcd6f54
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-06 09:46:07 -0300

    commit referente ao artefato utilitarios.
    -> Refatoração do código.
    
    -> Renomeação da classe.

---
+++ utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/CartaoCreditoFormatter.java
@@ -0,0 +1,46 @@
+package br.com.codetisolutions.arquitetura.formatadores;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> CartaoFormatter.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b> Classe responsável por formatar os números de cartões de créditos.
+ * </p>
+ *
+ * Data de criação: 06/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public final class CartaoCreditoFormatter extends Formatter {
+
+	/**
+	 * Responsável pela criação de novas instâncias desta classe.
+	 */
+	private CartaoCreditoFormatter() {
+
+		super();
+	}
+
+	/**
+	 * Método responsável por formatar o cartão de crédito.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param numeroCartao
+	 *            - string que representa o número do cartão de crédito.
+	 * 
+	 * @return <i>cartão formatado</i>
+	 */
+	public static String formatarCartaoCredito(final String numeroCartao) {
+
+		return null;
+	}
+}
