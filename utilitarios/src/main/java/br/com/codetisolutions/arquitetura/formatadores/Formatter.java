232c938ff7d961adfb7d1111b39e23beb8bdcb32
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-06 09:46:29 -0300

    commit referente ao artefato utilitarios.
    -> Implementação da classe Formatter.

--- utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/Formatter.java
+++ utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/Formatter.java
@@ -1,5 +1,7 @@
 package br.com.codetisolutions.arquitetura.formatadores;
 
+import javax.swing.JFormattedTextField;
+
 /**
  * <p>
  * <b>Organização:</b> 4code TI Solutions
@@ -20,5 +22,35 @@ package br.com.codetisolutions.arquitetura.formatadores;
  * @version 1.0.0
  */
 public class Formatter {
+
+	/**
+	 * Responsável pela criação de novas instâncias desta classe.
+	 */
+	protected Formatter() {
+
+		super();
+	}
+
+	/**
+	 * Método responsável por formatar uma string de acordo com o pattern parametrizado.
+	 *
+	 * @author marcosbuganeme
+	 *
+	 * @param string
+	 *            - String que será formatada.
+	 * 
+	 * @param pattern
+	 *            - pattern que será utilizado na formatação.
+	 * 
+	 * @return <i>string formatada de acordo com o pattern repassado</i>.
+	 */
+	protected static String formatarString(final String string, final String pattern) {
+
+		final JFormattedTextField formatar = new JFormattedTextField(pattern);
+
+		formatar.setText(string);
+
+		return formatar.getText();
+	}
 
 }
