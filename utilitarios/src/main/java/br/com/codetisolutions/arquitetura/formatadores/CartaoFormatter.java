5dcb8f576c3bc04d92e3db4f671e6758c7fdcb9f
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-06 09:33:37 -0300

    commit referente ao artefato utilitarios.
    -> Adição da classe CartaoFormatter.

---
+++ utilitarios/src/main/java/br/com/codetisolutions/arquitetura/formatadores/CartaoFormatter.java
@@ -0,0 +1,24 @@
+package br.com.codetisolutions.arquitetura.formatadores;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> CartaoFormatter.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 06/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class CartaoFormatter {
+
+}
