4c2907b21c13f63b53fab429dbf330a7aaf94888
marcos olavo silva buganeme <marcos.after@gmail.com>
2014-08-05 21:31:58 -0300

    commit referente ao artefato arquitetura-jsf.
    -> Criação da classe conversora de objetos do tipo comboBox.

---
+++ arquitetura-jsf/src/main/java/br/com/codetisolutions/arquitetura/jsf/converters/ComboBoxConverter.java
@@ -0,0 +1,40 @@
+package br.com.codetisolutions.arquitetura.jsf.converters;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/**
+ * <p>
+ * <b>Organização:</b> 4code TI Solutions
+ * </p>
+ *
+ * <p>
+ * <b>Título:</b> ComboBoxConverter.java
+ * </p>
+ *
+ * <p>
+ * <b>Descrição:</b>
+ * </p>
+ *
+ * Data de criação: 05/08/2014
+ *
+ * @author marcosbuganeme
+ *
+ * @version 1.0.0
+ */
+public class ComboBoxConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+
+		return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+
+		return null;
+	}
+
+}
